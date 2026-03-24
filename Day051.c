4fbef53fc07c2fb9fcf22a837aba064438189473
PrakharGoyal-coder <prakhargoyal740@gmail.com>
2026-03-24 12:41:06 +0530

    Uploaded

---
+++ Day051.c
@@ -0,0 +1,103 @@
+/*Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value
+
+Example:
+Input:
+7
+6 2 8 0 4 7 9
+2 8
+
+Output:Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value
+
+Example:
+Input:
+7
+6 2 8 0 4 7 9
+2 8
+
+Output:
+6
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Find LCA
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    if (root == NULL)
+        return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return LCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return LCA(root->right, n1, n2);
+
+    return root;
+}
+
+int main() {
+    int n, val;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* ans = LCA(root, n1, n2);
+
+    if (ans != NULL)
+        printf("%d\n", ans->data);
+
+    return 0;
+}
